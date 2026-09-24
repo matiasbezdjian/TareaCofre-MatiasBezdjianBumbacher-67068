662bcbdb46b3fc437ecfd8dee78ea8e4291972b3
matiasbezdjian <matiasbezdjian@gmail.com>
2026-09-23 21:33:35 -0300

    TareaCofre Matias Bezdjian Bumbacher

---
+++ Tarea cofre MatiasBezdjian/Tarea cofre MatiasBezdjian/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tarea_cofre_MatiasBezdjian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CofreMadera cofre = new CofreMadera();
+
+            int oroDado = cofre.Abrir();
+            Console.WriteLine("Encontraste " + oroDado + " de oro.");
+        }
+    }
+
+    class CofreMadera
+    {
+        public int Oro = 75;
+        public bool abierto = false;
+
+        public int Abrir()
+        {
+            if (abierto == false)
+            {
+                abierto = true;
+                return Oro;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
